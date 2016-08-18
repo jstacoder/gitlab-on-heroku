97da70ace0e3a49794277164a84a9cf7013906e0
Kyle Roux <jstacoder@gmail.com>
2016-08-18 12:25:12 -0700

    fixs

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,6 @@
 echo "running command: gem install --verbose charlock_holmes_bundle_icu -v '0.6.9.2'"
 #gem install --verbose charlock_holmes_bundle_icu -v '0.6.9.2'
-gem install --verbose charlock_holmes -v '0.7.3'
+#gem install --verbose charlock_holmes -v '0.7.3'
+gem install --verbose charlock_holmes -v '0.6.9'
 #gem install --verbose bundler
 #bundle install
